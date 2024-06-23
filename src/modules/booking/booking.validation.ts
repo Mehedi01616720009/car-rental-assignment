8cdfd0e39e23e93484dba2fd85492a48a16ec1a7
Mehedi01616720009 <mehedi01616720009@gmail.com>
2024-06-23 17:00:48 +0600

    validation bug fixing

--- src/modules/booking/booking.validation.ts
+++ src/modules/booking/booking.validation.ts
@@ -16,6 +16,9 @@ const bookingSetValidationSchema = z.object({
 
 const bookingEndValidationSchema = z.object({
     body: z.object({
+        bookingId: z.string({
+            required_error: 'Booking ID is required',
+        }),
         endTime: z.string({
             required_error: 'End Time is required',
         }),
