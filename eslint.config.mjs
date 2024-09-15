d33bd79576306ba5d57990b15a4841ba841dc877
Mehedi01616720009 <mehedi01616720009@gmail.com>
2024-09-15 20:38:07 +0600

    eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -17,6 +17,7 @@ export default [
             'prefer-const': 'error',
             'no-console': 'warn',
             'no-undef': 'error',
+            '@typescript-eslint/no-this-alias': 'none',
         },
     },
 
