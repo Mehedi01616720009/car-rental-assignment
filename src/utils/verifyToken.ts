e7a13f6d56e1d3c0f43e122190b51efdfda77e3d
Mehedi01616720009 <mehedi01616720009@gmail.com>
2024-09-18 12:36:58 +0600

    verify token

--- src/utils/verifyToken.ts
+++ src/utils/verifyToken.ts
@@ -1,10 +1,12 @@
 import jwt, { JwtPayload } from 'jsonwebtoken';
+import AppError from '../errors/AppError';
+import httpStatus from 'http-status';
 
 const verifyToken = (token: string, secret: string) => {
     try {
         return jwt.verify(token, secret) as JwtPayload;
     } catch (err: any) {
-        return err;
+        throw new AppError(httpStatus.UNAUTHORIZED, 'You are not authorized');
     }
 };
 
